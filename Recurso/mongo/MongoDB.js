1c10722f800e7a8c316fcfd8ce793ec3a8ff84ec
BensonTerra <benson.terra@hotmail.com>
2025-02-25 19:50:36 +0000

    save

--- Recurso/mongo/MongoDB.js
+++ Recurso/mongo/MongoDB.js
@@ -1,7 +1,32 @@
-db.moveies.find({countries:{$in:["USA"]}},{_id:1,title:1,countries:1}).limit(3);
+db.movies.find(
+    {countries:{$in:["USA"]}},
+    {_id:1,title:1,countries:1}
+).limit(3);
 
-db.movies.find({$and:[{type:"series"},{languages:{$in:["Japanese"]}}]},{_id:0,title:1,languages:1,type:1}).limit(3);
+db.movies.find(
+    {$and:[{type:"series"},{languages:{$in:["Japanese"]}}]},
+    {_id:0,title:1,languages:1,type:1}
+).limit(3);
 
-db.moveies.find({rating:{$gte:7.5,$lte:8.5}},{_id:1,title:1,rating:1,countries:1}).limit(3);
+db.movies.find(
+    {rating:{
+        $gte:7.5,
+        $lte:8.5
+        }
+    },
+    {
+        _id:1,
+        title:1,
+        rating:1,
+        countries:1
+    }
+).limit(3);
 
-db.moveies.find({genres:{$in:["Adventure"]}},{_id:1,title:1,countries:1}).limit(3);
+db.movies.find(
+    {genres:{$in:["Adventure"]}},
+    {_id:1,title:1,countries:1}
+).limit(3);
+
+
+GROUP BY 
+    DATE(rental_date)  
--- Recurso/sql/revisao_v2.sql
+++ Recurso/sql/revisao_v2.sql
@@ -225,3 +225,83 @@ DELIMITER ;
 -- Execução da stored procedure para listar o número de filmes na língua "Japanese":
 CALL GetNumFilmByLang("Japanese");
 
+
+-- parte 2
+
+-- a) Quais os nomes dos filmes que têm a palavra "super" no título?
+SELECT t1.title
+FROM film t1
+WHERE t1.title LIKE '%super%';
+
+-- b) Quais os filmes (title, release_year) de animação com duração (length) superior a 120 minutos?
+SELECT t1.title, t1.release_year
+FROM film t1
+JOIN film_category t2 ON t1.film_id = t2.film_id
+JOIN category t3 ON t2.category_id = t3.category_id
+WHERE t3.name = 'Animation' AND t1.length > 120;
+
+-- c) Listar os filmes (title, release_year) em que entra o ator "Henry Fonda":
+SELECT t1.title, t1.release_year
+FROM film t1
+JOIN film_actor t2 ON t1.film_id = t2.film_id
+JOIN actor t3 ON t2.actor_id = t3.actor_id
+WHERE t3.first_name = 'Henry' AND t3.last_name = 'Fonda';
+
+-- d) Listar as possíveis durações de aluguer (rental_duration) dos filmes registados:
+SELECT DISTINCT t1.rental_duration
+FROM film t1;
+
+-- e) Lista de clientes (first_name, last_name, email), ordenada alfabeticamente, que moram em Paris, França:
+SELECT t1.first_name, t1.last_name, t1.email
+FROM customer t1
+JOIN address t2 ON t1.address_id = t2.address_id
+JOIN city t3 ON t2.city_id = t3.city_id
+JOIN country t4 ON t3.country_id = t4.country_id
+WHERE t3.city = 'Paris' AND t4.country = 'France'
+ORDER BY t1.first_name, t1.last_name;
+
+-- f) Número médio de alugueres por dia em 2023:
+SELECT AVG(rentals_per_day) AS avg_rentals_per_day
+FROM (
+    SELECT DATE(t1.rental_date) AS rental_day, COUNT(t1.rental_id) AS rentals_per_day
+    FROM rental t1
+    WHERE YEAR(t1.rental_date) = 2023
+    GROUP BY rental_day
+) AS daily_rentals;
+
+-- g) Número de alugueres em cada dia de dezembro de 2023:
+SELECT DATE(t1.rental_date) AS rental_day, COUNT(t1.rental_id) AS rentals
+FROM rental t1
+WHERE YEAR(t1.rental_date) = 2023 AND MONTH(t1.rental_date) = 12
+GROUP BY rental_day
+ORDER BY rental_day;
+
+-- h) Lista das cidades com mais de 5 lojas e número de lojas em cada uma dessas cidades. Dados ordenados por número decrescente de lojas:
+SELECT t2.city, COUNT(t1.store_id) AS num_lojas
+FROM store t1
+JOIN address t3 ON t1.address_id = t3.address_id
+JOIN city t2 ON t3.city_id = t2.city_id
+GROUP BY t2.city
+HAVING COUNT(t1.store_id) > 5
+ORDER BY num_lojas DESC;
+
+-- i) Registar a cidade de Vila do Conde em Portugal:
+INSERT INTO city (city, country_id)
+VALUES ('Vila do Conde', (SELECT t2.country_id FROM country t2 WHERE t2.country = 'Portugal'));
+
+-- j) Registar que o cliente 1001 fez o pagamento de €4.99 relativamente ao aluguer 54321, tendo sido atendido pelo funcionário 123:
+INSERT INTO payment t1 (customer_id, staff_id, rental_id, amount)
+VALUES (1001, 123, 54321, 4.99);
+
+-- k) Atualizar todas as lojas de Vila do Conde, Portugal, que passam a ser geridas pelo funcionário "Joe Smith" com o email jsmith@mystore.com:
+UPDATE store t1
+JOIN address t2 ON t1.address_id = t2.address_id
+JOIN city t3 ON t2.city_id = t3.city_id
+JOIN staff t4 ON t1.manager_staff_id = t4.staff_id
+SET t1.manager_staff_id = (SELECT t5.staff_id FROM staff t5 WHERE t5.first_name = 'Joe' AND t5.last_name = 'Smith' AND t5.email = 'jsmith@mystore.com')
+WHERE t3.city = 'Vila do Conde' AND t3.country_id = (SELECT t6.country_id FROM country t6 WHERE t6.country = 'Portugal');
+
+-- l) Eliminar o pagamento de €5.99 relativo ao aluguer 54321, do cliente 1001, registado pelo funcionário 123:
+DELETE t1 FROM payment t1
+WHERE t1.amount = 5.99 AND t1.rental_id = 54321 AND t1.customer_id = 1001 AND t1.staff_id = 123;
+
